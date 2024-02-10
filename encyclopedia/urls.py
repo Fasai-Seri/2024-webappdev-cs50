d858970a76c8977ff08b0b42295ee7215fec16ab
Fasai Serikijcharoen <fasai.seri@gmail.com>
2024-02-10 10:36:46 +0700

    try search but not finished yet

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:entry>", views.show, name="show")
+    path("<str:entry>", views.show, name="show"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render
-
+from django import forms
+from django.http import HttpResponseRedirect
 from . import util
 import markdown2
 
+class NewSearchForm(forms.Form):
+    search = forms.CharField(label="Search")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "form": NewSearchForm()
     })
 
 def show(request, entry):
     return render(request, "encyclopedia/show.html", {
         "entry": entry,
-        "details": markdown2.markdown(util.get_entry(entry))
+        "details": markdown2.markdown(util.get_entry(entry)),
+        "form": NewSearchForm(),
     })
+    
+def search(request):
+    search = request.GET['search']
+    print(search)
+    if  search in util.list_entries():
+        return render(request, "encyclopedia/search.html", {
+            "test": search
+        })
+
+
+    
+                
