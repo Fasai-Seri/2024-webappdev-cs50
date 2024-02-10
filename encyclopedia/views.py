9a5bd4e96d8c5b5146556292396c0a384a832c7f
Fasai Serikijcharoen <fasai.seri@gmail.com>
2024-02-10 16:19:22 +0700

    finish create new page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django import forms
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from . import util
 import markdown2
 
+
 class NewSearchForm(forms.Form):
     search = forms.CharField(label="Search")
 
-# class NewPageForm(forms.Form):
-#     title = forms.CharField(label="Title"),
-#     te
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title")
+    details = forms.CharField(widget=forms.Textarea())
+    
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
@@ -35,13 +37,32 @@ def search(request):
             "entries": find,
             "form": NewSearchForm()
         })
-    
-def create(request):
-    return render(request, "encyclopedia/create.html", {
-        "form": NewSearchForm()
-    })
-        
 
-
+def create(request, exist=[entry.lower() for entry in util.list_entries()]):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            details = form.cleaned_data["details"]
+            if title.lower() in exist:
+                return render(request, "encyclopedia/create.html", {
+                "form": NewSearchForm(),
+                "page": form,
+                "error": 'This title is already existed'
+            })
+            else:
+                util.save_entry(title, details)
+                return HttpResponseRedirect(reverse('show', args=[title]))
+        else:
+            return render(request, "encyclopedia/create.html", {
+                "form": NewSearchForm(),
+                "page": form,
+                "error": 'The form is not valid'
+            })
+    else:
+        return render(request, "encyclopedia/create.html",{
+            "form": NewSearchForm(),
+            "page": NewPageForm()
+        })
     
                 
