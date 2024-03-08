9ec6237a5dc4106ef065ebcff9ebb2e216375f5a
Fasai Serikijcharoen <fasai.seri@gmail.com>
2024-03-08 12:39:44 +0700

    finish create listing

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,34 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    pass
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=64, default='')
+    
+    def __str__(self):
+        return f'{self.name}'
+    
+class AuctionListing(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=64)
+    description = models.TextField(default='')
+    starting_bid = models.FloatField(default=0)
+    image_url = models.URLField(blank=True)
+    category = models.ForeignKey(Category, related_name='listing_by_category', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f'{self.title}'
+    
+class Bid(models.Model):
+    id = models.AutoField(primary_key=True)
+    pass
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
     pass
--- auctions/views.py
+++ auctions/views.py
@@ -3,10 +3,17 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, Category, AuctionListing
 
-
+class CreateLisingForm(forms.Form):
+    title = forms.CharField(label='Title:')
+    description = forms.CharField(widget=forms.Textarea(), label='Description:')
+    starting_bid = forms.FloatField(label='Starting bid:')
+    image_url = forms.URLField(required=False, label='URL for image (optional):')
+    category = forms.ChoiceField(widget=forms.Select, choices=[(cat.name, cat.name) for cat in Category.objects.all()])
+    
 def index(request):
     return render(request, "auctions/index.html")
 
@@ -61,3 +68,22 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+    
+def create_listing(request):
+    if request.method == 'POST':
+        form = CreateLisingForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            starting_bid = form.cleaned_data['starting_bid']
+            image_url = form.cleaned_data['image_url']
+            category = Category.objects.get(name=form.cleaned_data['category'])
+            owner = request.user
+            is_active = True
+            new_listing = AuctionListing(title=title, description=description, starting_bid=starting_bid, image_url=image_url, category=category, owner=owner, is_active=is_active)
+            new_listing.save()
+        
+           
+    return render(request, 'auctions/create_listing.html', {
+        'form': CreateLisingForm(),
+    })
