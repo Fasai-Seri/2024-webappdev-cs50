d3f73f8e2c5b061a4967d60ff7311ac4c62e86f0
Fasai Serikijcharoen <fasai.seri@gmail.com>
2024-03-21 21:57:05 +0700

    finish html for new post

--- network/models.py
+++ network/models.py
@@ -3,4 +3,12 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    id = models.AutoField(primary_key=True)
+    following = models.ManyToManyField('self', related_name='followed')
+    
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    poster = models.ForeignKey(User, related_name='post', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField()
+    like_user = models.ManyToManyField(User, related_name='liked_post')
